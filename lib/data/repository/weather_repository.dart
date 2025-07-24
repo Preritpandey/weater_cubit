9022383303f5ca9a506717983a46075b7bc8d079
PreritPandey <pandeyprerit45@gmail.com>
2025-07-24 12:35:27 +0545

    fix: fixed UI bugs and uneven padding

---
+++ lib/data/repository/weather_repository.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:weather_bloc/data/provider/weather_data_provider.dart';
+
+class WeatherRepository {
+  final WeatherDataProvider weatherDataProvider;
+
+  WeatherRepository(this.weatherDataProvider);
+  Future<Map<String, dynamic>> getCurrentWeather() async {
+    try {
+      String cityName = 'London';
+
+      final weatherData = await weatherDataProvider.getCurrentWeather(cityName);
+      final data = jsonDecode(weatherData);
+
+      if (data['cod'] != '200') {
+        throw 'An unexpected error occurred';
+      }
+
+      return data;
+    } catch (e) {
+      throw e.toString();
+    }
+  }
+}
